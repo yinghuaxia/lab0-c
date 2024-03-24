51127bdedf9c1fc505a86cf6d79385273c283b24
yinghuaxia <yinghuayeh0521@gmail.com>
2024-03-24 19:54:04 +0800

    Compare execution time for two sorting
    Test if timsort perform better with a linked list that is partially sorted by using reverseK function

--- timsort.h
+++ timsort.h
@@ -5,3 +5,7 @@ typedef int (*list_cmp_func_t)(void *,
                                const struct list_head *);
 
 void timsort(void *priv, struct list_head *head, list_cmp_func_t cmp);
+
+__attribute__((nonnull(1, 2))) int cmp(void *priv,
+                                       const struct list_head *a,
+                                       const struct list_head *b);
